35d813e3736adeb17c166e738688e473c49255de
DemirGokdemir <demir.gokdemir@std.yeditepe.edu.tr>
2025-11-01 17:01:15 +0300

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+import numpy as np 
+def find_period(L0, L1):
+    #this function calculate and print pendulum period T for lengths from L0 to L1
+    #for 1 meter increments
+    g = 9.81 # in m/s^2
+    for L in range(L0, L1+1, 1):
+        T = 2 * np.pi * np.sqrt(L / g)
+        print(f"When L= {L:.1f} m, T = {T:.1f} s")
+        
+    T0 = 2 * np.pi * np.sqrt(L0 / g) #in s
+    T1 = 2 * np.pi * np.sqrt(L1 / g) #in s
+    return T0,T1
+myresult = find_period(2, 10)
+
+
